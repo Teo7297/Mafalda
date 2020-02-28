5dfbfc45521b6eec821c21c2c29a090e86f7e8f6
Matteo <teocava7297@gmail.com>
2020-02-28 16:49:42 +0100

    adding just created project code with right settings, Spring boot version 2.2.5, dependencies added: mongodb (both)

---
+++ code/Mafalda/src/main/java/com/application/mafalda/MafaldaApplication.java
@@ -0,0 +1,13 @@
+package com.application.mafalda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafaldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafaldaApplication.class, args);
+	}
+
+}
---
+++ code/Mafalda/src/test/java/com/application/mafalda/MafaldaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.mafalda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafaldaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
