e4d8fdaf8dd4924296e3208a0f9ac3729c41e683
odococo <ivan_lamperti@outlook.it>
2020-03-15 21:00:44 +0100

    calendar to get field of date

--- code/Mafalda/src/main/java/com/database/mock/Personnels.java
+++ code/Mafalda/src/main/java/com/database/mock/Personnels.java
@@ -1,6 +1,7 @@
 package com.database.mock;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.database.classes.Personnel;
@@ -19,7 +20,9 @@ public class Personnels {
 			Date assignment = random.randomDate(2018, 2020, 1, 12, 1, 28);
 			Personnel p = new Personnel(user.getUsername(), role, assignment);
 			if (random.randomBoolean()) {
-				p.setRemoved(random.randomDate(assignment.getYear(), assignment.getYear() + random.randomInt(3), assignment.getMonth(), 12, assignment.getDay(), 28));
+				Calendar calendar = Calendar.getInstance();
+				calendar.setTime(assignment);
+				p.setRemoved(random.randomDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.YEAR) + random.randomInt(3), calendar.get(Calendar.MONTH), 12, calendar.get(Calendar.DAY_OF_MONTH), 28));
 			}
 			personnels.add(p);
 		}
